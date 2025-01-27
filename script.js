e480a0e07fc8edc2ea9c5f1de012d27b1e4cf754
notxtr <s10267163@connect.np.edu.sg>
2025-01-27 13:54:22 +0800

    changed

--- script.js
+++ script.js
@@ -1,43 +1,217 @@
-document.getElementById('dataForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    // Get the form data
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-
-    // Prepare data to be sent to restdb.io
-    const data = {
-        name: name,
-        email: email
-    };
-
-    // API endpoint URL (replace YOUR_COLLECTION with your actual collection name)
-    const apiUrl = 'https://mokesell-1e1c.restdb.io/rest/system_jobs';
-
-    // API key from restdb.io
-    const apiKey = '679717f9f9d2bbe70d181e2e';  // Replace with your actual API key
-
-    // Send data using fetch
-    fetch(apiUrl, {
-        method: 'POST',
+//[STEP 0]: Make sure our document is A-OK
+document.addEventListener("DOMContentLoaded", function () {
+    // What kind of interface we want at the start 
+    const APIKEY = "679717f9f9d2bbe70d181e2e";
+    getContacts();
+    document.getElementById("update-contact-container").style.display = "none";
+    document.getElementById("add-update-msg").style.display = "none";
+  
+    //[STEP 1]: Create our submit form listener
+    document.getElementById("contact-submit").addEventListener("click", function (e) {
+      // Prevent default action of the button 
+      e.preventDefault();
+  
+      //[STEP 2]: Let's retrieve form data
+      // For now, we assume all information is valid
+      // You are to do your own data validation
+      let contactName = document.getElementById("contact-name").value;
+      let contactEmail = document.getElementById("contact-email").value;
+      let contactMessage = document.getElementById("contact-msg").value;
+  
+      //[STEP 3]: Get form values when the user clicks on send
+      // Adapted from restdb API
+      let jsondata = {
+        "name": contactName,
+        "email": contactEmail,
+        "message": contactMessage
+      };
+  
+      //[STEP 4]: Create our AJAX settings. Take note of API key
+      let settings = {
+        method: "POST", //[cher] we will use post to send info
         headers: {
-            'Content-Type': 'application/json',
-            'x-apikey': apiKey,
-            'Cache-Control': 'no-cache'
+          "Content-Type": "application/json",
+          "x-apikey": APIKEY,
+          "Cache-Control": "no-cache"
         },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        alert('Data submitted successfully!');
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Failed to submit data.');
-    });
-});
-
+        body: JSON.stringify(jsondata),
+        beforeSend: function () {
+          //@TODO use loading bar instead
+          // Disable our button or show loading bar
+          document.getElementById("contact-submit").disabled = true;
+          // Clear our form using the form ID and triggering its reset feature
+          document.getElementById("add-contact-form").reset();
+        }
+      }
+  
+      //[STEP 5]: Send our AJAX request over to the DB and print response of the RESTDB storage to console.
+      fetch("https://mokesell-1e1c.restdb.io/rest/system_jobs", settings)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          document.getElementById("contact-submit").disabled = false;
+          //@TODO update frontend UI 
+          document.getElementById("add-update-msg").style.display = "block";
+          setTimeout(function () {
+            document.getElementById("add-update-msg").style.display = "none";
+          }, 3000);
+          // Update our table 
+          getContacts();
+        });
+    });//end click 
+  
+  
+    //[STEP] 6
+    // Let's create a function to allow you to retrieve all the information in your contacts
+    // By default, we only retrieve 10 results
+    function getContacts(limit = 100, all = true) {
+  
+      //[STEP 7]: Create our AJAX settings
+      let settings = {
+        method: "GET", //[cher] we will use GET to retrieve info
+        headers: {
+          "Content-Type": "application/json",
+          "x-apikey": APIKEY,
+          "Cache-Control": "no-cache"
+        },
+      }
+  
+      //[STEP 8]: Make our AJAX calls
+      // Once we get the response, we modify our table content by creating the content internally. We run a loop to continuously add on data
+      // RESTDb/NoSql always adds in a unique id for each data; we tap on it to have our data and place it into our links 
+      fetch("https://mokesell-1e1c.restdb.io/rest/system_jobs", settings)
+        .then(response => response.json())
+        .then(response => {
+          let content = "";
+  
+          for (var i = 0; i < response.length && i < limit; i++) {
+            //console.log(response[i]);
+            //[METHOD 1]
+            // Let's run our loop and slowly append content
+            // We can use the normal string append += method
+            /*
+            content += "<tr><td>" + response[i].name + "</td>" +
+              "<td>" + response[i].email + "</td>" +
+              "<td>" + response[i].message + "</td>
+              "<td>Del</td><td>Update</td</tr>";
+            */
+  
+            //[METHOD 2]
+            // Using our template literal method using backticks
+            // Take note that we can't use += for template literal strings
+            // We use ${content} because -> content += content 
+            // We want to add on previous content at the same time
+            content = `${content}<tr id='${response[i]._id}'><td>${response[i].name}</td>
+            <td>${response[i].email}</td>
+            <td>${response[i].message}</td>
+            <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-contact-container' class='update' data-id='${response[i]._id}' data-msg='${response[i].message}' data-name='${response[i].name}' data-email='${response[i].email}'>Update</a></td></tr>`;
+  
+          }
+  
+          //[STEP 9]: Update our HTML content
+          // Let's dump the content into our table body
+          document.getElementById("contact-list").getElementsByTagName("tbody")[0].innerHTML = content;
+  
+          document.getElementById("total-contacts").innerHTML = response.length;
+        });
+    }
+  
+    //[STEP 10]: Create our update listener
+    // Here we tap onto our previous table when we click on update
+    // This is a delegation feature of jQuery
+    // Because our content is dynamic in nature, we listen in on the main container which is "#contact-list". For each row, we have a class .update to help us
+    document.getElementById("contact-list").addEventListener("click", function (e) {
+      if (e.target.classList.contains("update")) {
+        e.preventDefault();
+        // Update our update form values
+        let contactName = e.target.getAttribute("data-name");
+        let contactEmail = e.target.getAttribute("data-email");
+        let contactMsg = e.target.getAttribute("data-msg");
+        let contactId = e.target.getAttribute("data-id");
+        console.log(e.target.getAttribute("data-msg"));
+  
+        //[STEP 11]: Load in our data from the selected row and add it to our update contact form 
+        document.getElementById("update-contact-name").value = contactName;
+        document.getElementById("update-contact-email").value = contactEmail;
+        document.getElementById("update-contact-msg").value = contactMsg;
+        document.getElementById("update-contact-id").value = contactId;
+        document.getElementById("update-contact-container").style.display = "block";
+      }
+      else if (e.target.classList.contains("delete")) {
+        e.preventDefault();
+  
+        let contactId = e.target.dataset.id;
+        deleteRecord(contactId);
+      }
+    });//end contact-list listener for update function
+  
+    //[STEP 12]: Here we load in our contact form data
+    // Update form listener
+    document.getElementById("update-contact-submit").addEventListener("click", function (e) {
+      e.preventDefault();
+      // Retrieve all my update form values
+      let contactName = document.getElementById("update-contact-name").value;
+      let contactEmail = document.getElementById("update-contact-email").value;
+      let contactMsg = document.getElementById("update-contact-msg").value;
+      let contactId = document.getElementById("update-contact-id").value;
+  
+      console.log(document.getElementById("update-contact-msg").value);
+      console.log(contactMsg);
+  
+      //[STEP 12a]: We call our update form function which makes an AJAX call to our RESTDB to update the selected information
+      updateForm(contactId, contactName, contactEmail, contactMsg);
+    });//end updatecontactform listener
+  
+    //[STEP 13]: Function that makes an AJAX call and processes it 
+    // UPDATE Based on the ID chosen
+    function updateForm(id, contactName, contactEmail, contactMsg) {
+      //@TODO create validation methods for id etc. 
+  
+      var jsondata = { "name": contactName, "email": contactEmail, "message": contactMsg };
+      var settings = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "x-apikey": APIKEY,
+          "Cache-Control": "no-cache"
+        },
+        body: JSON.stringify(jsondata)
+      }
+  
+      //[STEP 13a]: Send our AJAX request and hide the update contact form
+      fetch(`https://mokesell-1e1c.restdb.io/rest/system_jobs/${id}`, settings)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          document.getElementById("update-contact-container").style.display = "none";
+          // Update our contacts table
+          getContacts();
+        });
+  
+    }//end updateform function
+      //delete function
+      function deleteRecord(id) {
+        let settings = {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            "x-apikey": APIKEY,
+            "Cache-Control": "no-cache"
+          }
+        };
+    
+        fetch(`https://mokesell-1e1c.restdb.io/rest/system_jobs/${id}`, settings)
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            //update our contacts table
+            getContacts();
+          })
+          .catch(error => console.log(error));
+      }
+  
+  });
+  
 
 
 
