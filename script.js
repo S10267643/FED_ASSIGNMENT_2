8a1199b5f4eee0668238e308848c9568ed1809bd
S10267643 <s10267643@connect.np.edu.sg>
2025-02-03 13:17:23 +0800

    add trending

--- script.js
+++ script.js
@@ -275,7 +275,13 @@ function fetchGameData() {
 }
   
 
+document.getElementById('scroll-left').addEventListener('click', function() {
+  document.getElementById('trending-items').scrollBy({ left: -200, behavior: 'smooth' });
+});
 
+document.getElementById('scroll-right').addEventListener('click', function() {
+  document.getElementById('trending-items').scrollBy({ left: 200, behavior: 'smooth' });
+});
 
 
 
