ab02de84a59385f1151a4b18116890ad28f217ea
S10267643 <s10267643@connect.np.edu.sg>
2025-02-07 01:38:56 +0800

    f

--- signup.js
+++ signup.js
@@ -1,111 +1,7 @@
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const APIKEY = "67a057fa417fee624eb30f33";
-//     const signupForm = document.getElementById("signup");
-
-//     signupForm.addEventListener("submit", async function (event) {
-//         event.preventDefault();
-//         const email = document.getElementById("email").value.trim();
-//         const password = document.getElementById("password").value.trim();
-//         const name= document.getElementById("name").value.trim();
-        
-
-//        
-//        
-
-       
-
-//         const set = {
-//             method: "GET",
-//             headers: {
-//                 "Content-Type": "application/json",
-//                 "x-apikey": APIKEY,
-//                 "Cache-Control": "no-cache",
-//             },
-//         };
-//         const check = await fetch("https://mokesell-536e.restdb.io/rest/accounts", set);
-//         const data = await check.json();
-
-        
-
-//         const message = {
-//             "email": email,
-//             "password": password,
-//             'name': name,
-            
-//         };
-
-//         for (let i of data) {
-//             if (i.email === email) {
-//                 alert("This email has been registered!");
-//                 return;
-//             }
-//         }
-
-//         const settings = {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//                 "x-apikey": APIKEY,
-//                 "Cache-Control": "no-cache",
-//             },
-//             body: JSON.stringify(message),
-//         };
-
-        
-//     });
-// });
 
 
-document.addEventListener("DOMContentLoaded", function(){
-    APIKEY="67a057fa417fee624eb30f33";
-    
-         let loginForm = document.getElementById("login")
-     login.addEventListener("submit",  async  function (event){
-    event.preventDefault();
-    let email = document.getElementById("email").value
-    let password = document.getElementById("password").value
-    
-     if (!email || !passwd) {
-         alert("Email and Password cannot be empty!");
-        return;
-     }
-    let settings = {
-         method:"GET",
-        headers:{
-          "Content-Type": "application/json",
-            "x-apikey": APIKEY,
-            "Cache-Control": "no-cache",
-            
-         }
-      };
-     try{                   
-      let search = await fetch("https://mokesell-536e.restdb.io/rest/accounts  ", settings)
-      
-      const data = await search.json();
-        let check = false;
-          for(let i of data){
-            if(i.email===email && i.password===password){
-                alert("Log in successful. Welcome to FindSell!")
-               check=true;
-               
-                localStorage.setItem("UserEmail",i.email)
-               localStorage.setItem("UserPassword",i.password)
-             
-                
-             break;
-            }
-          }
-       if(!check){
-           alert("Invalid email or password, try again.")
-        }
-     }
-        catch(error){
-        alert("Error Occurred during login. Please try again",error)
-    }
-    })
- })
-
+//signup 
 document.addEventListener("DOMContentLoaded", function () {
     const APIKEY = "67a057fa417fee624eb30f33";
 
@@ -123,22 +19,19 @@ document.getElementById("acct_submit").addEventListener("click", function (e) {
 
 
     if (!email || !password ) {
-                   alert("Email and Password fields cannot be empty!");
-                    return;
-               }
+        alert("Email and Password fields cannot be empty!");
+        return;}
 
     /* Check password length and if it contains at least one number*/
-         if (password.length < 8 || password.length > 20 || !/\d/.test(password)) {
-           alert("Password must be between 8 and 20 characters long and include at least one number.");
-             return;
-         }
+    if (password.length < 8 || password.length > 20 || !/\d/.test(password)) {
+        alert("Password must be between 8 and 20 characters long and include at least one number.");
+        return; }
 
-         for (let i of data) {
-                        if (i.email === email) {
-                            alert("This email has been registered!");
-                            return;
-                        }
-                     }
+    for (let i of jsondata) {
+        if (i.email === email) {
+        alert("This email has been registered!");
+        return;}}
+
     //[STEP 3]: Get form values when the user clicks on send
     // Adapted from restdb API
     let jsondata = {
@@ -172,13 +65,55 @@ document.getElementById("acct_submit").addEventListener("click", function (e) {
         console.log(data);
         document.getElementById("acct_submit").disabled = false;
         document.getElementById("signup").reset();
-       
-    
-        
       });
       
   });//end click 
-
-
-
 });
+
+
+
+//login
+document.addEventListener("DOMContentLoaded", function(){
+    APIKEY="67a057fa417fee624eb30f33";
+    
+    let loginForm = document.getElementById("login")
+    login.addEventListener("submit", async function (event){
+    event.preventDefault();
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    
+     if (!email || !password) {
+         alert("Email and Password cannot be empty!");
+        return;
+     }
+    let settings = {
+        method:"GET",
+        headers:{
+        "Content-Type": "application/json",
+        "x-apikey": APIKEY,
+        "Cache-Control": "no-cache",
+         }};
+     try{                   
+      let search = await fetch("https://mokesell-536e.restdb.io/rest/accounts  ", settings)
+      
+      const jsondata = await search.json();
+        let check = false;
+          for(let i of jsondata){
+            if(i.email===email && i.password===password){
+                alert("Log in successful. Welcome to MokeSell!")
+               check=true;
+               
+            localStorage.setItem("email",i.email)
+            localStorage.setItem("password",i.password)
+             break;
+            }
+          }
+       if(!check){
+           alert("Invalid email or password, try again.")
+        }
+     }
+        catch(error){
+        alert("Error Occurred during login. Please try again",error)
+    }
+    })
+ })
