9f15463b97043a3beeb420a81b74fe2d62dea8fc
S10267643 <s10267643@connect.np.edu.sg>
2025-02-07 01:26:55 +0800

    j

--- signup.js
+++ signup.js
@@ -10,16 +10,8 @@
 //         const name= document.getElementById("name").value.trim();
         
 
-//         if (!email || !password ) {
-//             alert("Email and Password fields cannot be empty!");
-//             return;
-//         }
-
-//         /* Check password length and if it contains at least one number*/
-//         if (password.length < 8 || password.length > 20 || !/\d/.test(password)) {
-//             alert("Password must be between 8 and 20 characters long and include at least one number.");
-//             return;
-//         }
+//        
+//        
 
        
 
@@ -63,55 +55,56 @@
         
 //     });
 // });
-// document.addEventListener("DOMContentLoaded", function(){
-//     APIKEY="67a057fa417fee624eb30f33";
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    APIKEY="67a057fa417fee624eb30f33";
     
+         let loginForm = document.getElementById("login")
+     login.addEventListener("submit",  async  function (event){
+    event.preventDefault();
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
     
-//     let loginForm = document.getElementById("login")
-//     login.addEventListener("submit",  async  function (event){
-//     event.preventDefault();
-//     let email = document.getElementById("email").value
-//     let password = document.getElementById("password").value
-    
-//     if (!email || !passwd) {
-//         alert("Email and Password cannot be empty!");
-//         return;
-//     }
-//     let settings = {
-//         method:"GET",
-//         headers:{
-//           "Content-Type": "application/json",
-//             "x-apikey": APIKEY,
-//             "Cache-Control": "no-cache",
+     if (!email || !passwd) {
+         alert("Email and Password cannot be empty!");
+        return;
+     }
+    let settings = {
+         method:"GET",
+        headers:{
+          "Content-Type": "application/json",
+            "x-apikey": APIKEY,
+            "Cache-Control": "no-cache",
             
-//         }
-//       };
-//       try{                   
-//       let search = await fetch("https://mokesell-536e.restdb.io/rest/accounts  ", settings)
+         }
+      };
+     try{                   
+      let search = await fetch("https://mokesell-536e.restdb.io/rest/accounts  ", settings)
       
-//       const data = await search.json();
-//         let check = false;
-//          for(let i of data){
-//             if(i.email===email && i.password===password){
-//                 alert("Log in successful. Welcome to FindSell!")
-//                 check=true;
+      const data = await search.json();
+        let check = false;
+          for(let i of data){
+            if(i.email===email && i.password===password){
+                alert("Log in successful. Welcome to FindSell!")
+               check=true;
                
-//                 localStorage.setItem("UserEmail",i.email)
-//                 localStorage.setItem("UserPassword",i.password)
+                localStorage.setItem("UserEmail",i.email)
+               localStorage.setItem("UserPassword",i.password)
              
                 
-//                 break;
-//             }
-//          }
-//          if(!check){
-//             alert("Invalid email or password, try again.")
-//          }
-//       }
-//     catch(error){
-//         alert("Error Occurred during login. Please try again",error)
-//     }
-//     })
-// })
+             break;
+            }
+          }
+       if(!check){
+           alert("Invalid email or password, try again.")
+        }
+     }
+        catch(error){
+        alert("Error Occurred during login. Please try again",error)
+    }
+    })
+ })
 
 document.addEventListener("DOMContentLoaded", function () {
     const APIKEY = "67a057fa417fee624eb30f33";
@@ -128,6 +121,24 @@ document.getElementById("acct_submit").addEventListener("click", function (e) {
     let email = document.getElementById("acct_email").value;
     let password = document.getElementById("acct_password").value;
 
+
+    if (!email || !password ) {
+                   alert("Email and Password fields cannot be empty!");
+                    return;
+               }
+
+    /* Check password length and if it contains at least one number*/
+         if (password.length < 8 || password.length > 20 || !/\d/.test(password)) {
+           alert("Password must be between 8 and 20 characters long and include at least one number.");
+             return;
+         }
+
+         for (let i of data) {
+                        if (i.email === email) {
+                            alert("This email has been registered!");
+                            return;
+                        }
+                     }
     //[STEP 3]: Get form values when the user clicks on send
     // Adapted from restdb API
     let jsondata = {
